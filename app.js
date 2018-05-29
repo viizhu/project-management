df608df0553f34cfb667633f034a08dec30bd7c5
Vicky Zhu <26657424+viizhu@users.noreply.github.com>
2018-05-29 19:17:02 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+    app     = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+//root route
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+//404 route
+app.get("*", function(req, res){
+  res.send("Sorry, page not found...");
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+  console.log("The Server has started");
+});
